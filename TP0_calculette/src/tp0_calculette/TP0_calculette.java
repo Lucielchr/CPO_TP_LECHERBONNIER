331fe3326dab3ba48c49800b02d7a948d57fa7ff
Lucie Lecherbonnier <lucie.lecherbonnier@epfedu.fr>
2025-10-22 12:53:54 +0200

    Création d'un nouveau projet TP0 calculette

---
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+
+/** calculer le resultat de l'operation impliquant les 2 operandes et l'operateur selectionne 
+ * 20/10/2025
+ * @author lucie
+ */
+public class TP0_calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+System.out.println("""
+                   1) add 
+                   2) substract 
+                   3) multiply 
+                   4) divide 
+                   5) modulo """) ;   }
+   
+}
