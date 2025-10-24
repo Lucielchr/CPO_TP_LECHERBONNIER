c0a67c4a7b7c0cd9ea96ad17ab72283123985f88
Lucie Lecherbonnier <lucie.lecherbonnier@epfedu.fr>
2025-10-24 12:52:40 +0200

    Création d'un nouveau projet TP1 convertisseur

---
+++ TP1_convertisseur_LECHERBONNIER/src/tp1_convertisseur_lecherbonnier/TP1_convertisseur_LECHERBONNIER.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_convertisseur_lecherbonnier;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lucie
+ */
+public class TP1_convertisseur_LECHERBONNIER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double var1; 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer le premier nombre: ");
+        var1=sc.nextDouble();
+    }
+    
+}
