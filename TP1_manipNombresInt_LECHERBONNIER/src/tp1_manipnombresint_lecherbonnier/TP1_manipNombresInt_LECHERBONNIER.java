f554ad77a902476288c973baab5a7d5fb1eaa546
Lucie Lecherbonnier <lucie.lecherbonnier@epfedu.fr>
2025-10-24 12:32:46 +0200

    Création d'un nouveau projet fin TP1 manip

--- TP1_manipNombresInt_LECHERBONNIER/src/tp1_manipnombresint_lecherbonnier/TP1_manipNombresInt_LECHERBONNIER.java
+++ TP1_manipNombresInt_LECHERBONNIER/src/tp1_manipnombresint_lecherbonnier/TP1_manipNombresInt_LECHERBONNIER.java
@@ -4,6 +4,8 @@
  */
 package tp1_manipnombresint_lecherbonnier;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lucie
@@ -14,7 +16,16 @@ public class TP1_manipNombresInt_LECHERBONNIER {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int var1; 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer le premier nombre: ");
+        var1=sc.nextInt();
+        int var2; 
+        System.out.println("Entrer le deuxieme nombre: ");
+        var2=sc.nextInt();
+        System.out.println(var1+var2);
+        System.out.println(var1/var2);
+        System.out.println(var1%var2);
     }
     
 }
