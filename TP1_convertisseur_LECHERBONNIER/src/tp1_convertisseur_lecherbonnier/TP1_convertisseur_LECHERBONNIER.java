9ae4a62c1f43149ce9269491ee29c2993ab5b683
Lucie Lecherbonnier <lucie.lecherbonnier@epfedu.fr>
2025-10-30 10:42:32 +0100

    suite et fin du TP1 convertisseur

--- TP1_convertisseur_LECHERBONNIER/src/tp1_convertisseur_lecherbonnier/TP1_convertisseur_LECHERBONNIER.java
+++ TP1_convertisseur_LECHERBONNIER/src/tp1_convertisseur_lecherbonnier/TP1_convertisseur_LECHERBONNIER.java
@@ -16,10 +16,53 @@ public class TP1_convertisseur_LECHERBONNIER {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double var1; 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entrer le premier nombre: ");
-        var1=sc.nextDouble();
+
+System.out.println("Bonjour, saisissez une valeur : ");
+double valeur = sc.nextDouble();
+
+System.out.println("\nSaisissez la conversion que vous souhaitez effectuer : ");
+System.out.println("1) De Celcius vers Kelvin");
+System.out.println("2) De Kelvin vers Celcius");
+System.out.println("3) De Celcius vers Farenheit");
+System.out.println("4) De Farenheit vers Celcius");
+System.out.println("5) De Kelvin vers Farenheit");
+System.out.println("6) De Farenheit vers Kelvin");
+
+int choix = sc.nextInt();
+double resultat = 0.0;
+
+switch (choix) {
+    case 1:
+        resultat = valeur + 273.15; 
+        System.out.println(valeur + " degres Celcius est egal a " + resultat + " degres Kelvin");
+        break;
+    case 2:
+        resultat = valeur - 273.15; 
+        System.out.println(valeur + " degres Kelvin est egal a " + resultat + " degres Celcius");
+        break;
+    case 3:
+        resultat = valeur * 9 / 5 + 32;
+        System.out.println(valeur + " degres Celcius est egal a " + resultat + " degres Farenheit");
+        break;
+    case 4:
+        resultat = (valeur - 32) * 5 / 9; 
+        System.out.println(valeur + " degres Farenheit est egal a " + resultat + " degres Celcius");
+        break;
+    case 5:
+        resultat = (valeur - 273.15) * 9 / 5 + 32;
+        System.out.println(valeur + " degres Kelvin est egal a " + resultat + " degres Farenheit");
+        break;
+    case 6:
+        resultat = (valeur - 32) * 5 / 9 + 273.15; 
+        System.out.println(valeur + " degres Farenheit est egal a " + resultat + " degres Kelvin");
+        break;
+    default:
+        System.out.println("Choix invalide !");
+}
+
+sc.close();
+        
     }
     
 }
