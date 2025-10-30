10d45e5011d7b73e83ca63f1ce1f66eb07e09961
Lucie Lecherbonnier <lucie.lecherbonnier@epfedu.fr>
2025-10-30 11:10:52 +0100

    fin TP1 stats a retravailler car aider par IA et pas tout compris

--- TP1_stats_LECHERBONNIER/src/tp1_stats_lecherbonnier/TP1_stats_LECHERBONNIER.java
+++ TP1_stats_LECHERBONNIER/src/tp1_stats_lecherbonnier/TP1_stats_LECHERBONNIER.java
@@ -4,6 +4,9 @@
  */
 package tp1_stats_lecherbonnier;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author lucie
@@ -14,7 +17,30 @@ public class TP1_stats_LECHERBONNIER {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int[] de = new int[6];
+
+        System.out.print("Combien de lancers voulez-vous effectuer ? ");
+        int m = sc.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            int face = rand.nextInt(6);
+            de[face]++;
+        }
+
+        System.out.println("\nNombre d'apparitions de chaque face :");
+        for (int i = 0; i < de.length; i++) {
+            System.out.println("Face " + (i + 1) + " : " + de[i]);
+        }
+
+        System.out.println("\nPourcentage d'apparition de chaque face :");
+        for (int i = 0; i < de.length; i++) {
+            double pourcentage = (de[i] * 100.0) / m;
+            System.out.printf("Face %d : %.2f%%\n", i + 1, pourcentage);
+        }
+
+        sc.close();
     }
-    
 }
