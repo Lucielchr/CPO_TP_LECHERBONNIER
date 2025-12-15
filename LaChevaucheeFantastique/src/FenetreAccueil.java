b612aba717fb517864376788704caba35f9ec5dc
Lucie Lecherbonnier <lucie.lecherbonnier@epfedu.fr>
2025-12-15 15:20:25 +0100

    Creation JeuChevaucheeFantastique

--- LaChevaucheeFantastique/src/FenetreAccueil.java
+++ LaChevaucheeFantastique/src/FenetreAccueil.java
@@ -28,6 +28,7 @@ public class FenetreAccueil extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -37,8 +38,20 @@ public class FenetreAccueil extends javax.swing.JFrame {
         jLabel1.setText("Bienvenu sur la chevauchée fantastique");
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 400, -1));
 
+        jButton1.setText("Niveau 1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 140, -1, -1));
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -66,6 +79,7 @@ public class FenetreAccueil extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
