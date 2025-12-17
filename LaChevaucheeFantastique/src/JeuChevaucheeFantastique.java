470a3f34e6459a821527e1ada1678a6367bfb021
Lucie Lecherbonnier <lucie.lecherbonnier@epfedu.fr>
2025-12-17 14:08:29 +0100

    JeuChevaucheeFantastique avance

--- LaChevaucheeFantastique/src/JeuChevaucheeFantastique.java
+++ LaChevaucheeFantastique/src/JeuChevaucheeFantastique.java
@@ -19,4 +19,50 @@ public class JeuChevaucheeFantastique {
     private Case posCavalier;
     private int niveauActuel = 0;
     
+    private static final int[][] MOUVEMENTS_CAVALIER = {
+        {-2, -1}, {-2, 1}, {-1, -2}, {-1, 2},
+        { 2, -1}, { 2, 1}, { 1, -2}, { 1, 2}
+    };
     
+    private static final int[][][] NIVEAUX_PREDEFINIS = {
+        {
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 1, 1, 1, 1},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0}
+        },
+        {
+            {1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {1, 0, 0, 0, 1}
+        }
+    };
+    
+    
+    public void passerAuNiveauSuivant() {
+        initialiserNiveau(this.niveauActuel + 1);
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+    
+    public int getEtatCase(int r, int c) {
+        return damier[r][c];
+    }
+
+    public Case getPosCavalier() {
+        return posCavalier;
+    }
+
+    public int getNiveauActuel() {
+        return niveauActuel;
+    }
+    
+    public int getTotalNiveaux() {
+        return NIVEAUX_PREDEFINIS.length;
+    }
+}
