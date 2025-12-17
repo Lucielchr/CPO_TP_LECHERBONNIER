9ab187447aac6cb6da693156f95798180f3a28b4
Lucie Lecherbonnier <lucie.lecherbonnier@epfedu.fr>
2025-12-17 15:14:01 +0100

    creation classe cellule

---
+++ miniprojet_cadenas_LECHERBONNIER/src/CelluleGraphique.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lucie
+ */
+public class CelluleGraphique {
+    private int etat;
+    private boolean estCavalier;
+    private boolean estClaire;
+    
+    
+    
+}
